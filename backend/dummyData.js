5cce4fdc8a1c1cabf65f1614db6949eba34eb574
bhavyajain-prog <jainbhavya2306@gmail.com>
2025-03-01 15:37:24 +0530

    Integration1

---
+++ backend/dummyData.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const User = require("./models/User");
+const Student = require("./models/Student");
+
+
+// Project Bank -- later csv
+// Student data -- csv
+// User data -- csv
+// Mentor data -- csv
+// Team data -- frontend
+// Evaluation data -- later frontend
